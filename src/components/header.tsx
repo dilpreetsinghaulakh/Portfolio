da81bd30d734a570003b083c461d29634cbdf860
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-05-11 16:05:35 +0530

    Update header styling and remove unnecessary link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,13 @@
 export default function Header() {
   return (
     <>
-      <header className=" z-10 fixed text-2xl w-[90vw] mx-[5vw] mt-3 px-8 rounded-2xl outline-black outline-[1px] outline backdrop-blur-2xl backdrop-opacity-[98%]">
+      <header className="bg-white/70 z-10 fixed text-2xl w-[90vw] mx-[5vw] mt-3 px-8 rounded-2xl outline-black bg- outline-[1px] outline backdrop-blur-sm">
         <div className="flex justify-between max-w-5xl py-3 mx-auto">
           <div>
             <a href="/" className=" font-bold">ds.</a>
           </div>
           <div className="r font-light text-[.75em]">
             <a href="/">Home</a>
-            <a href="/blog">Blog</a>
             <a href="/about">About</a>
           </div>
         </div>
