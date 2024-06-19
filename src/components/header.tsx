1328bd4981ec551590805ff7f310c6cfa5ca6d41
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-06-19 16:11:38 +0530

    refactor: Update header component with style, new logo and menu icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,51 @@
+"use client";
+import Logo from "../../public/logo.svg";
+
 export default function Header() {
+  const MenuIcon = (
+    <svg className="w-8 h-8 xl:w-12 xl:h-12" viewBox="0 0 50 50">
+      <line
+        x1="5"
+        y1="15.5"
+        x2="45"
+        y2="15.5"
+        stroke="currentcolor"
+        strokeWidth="3"
+      />
+      <line
+        x1="5"
+        y1="31.5"
+        x2="45"
+        y2="31.5"
+        stroke="currentcolor"
+        strokeWidth="3"
+      />
+    </svg>
+  );
   return (
     <>
-      <header className="bg-white/70 z-10 fixed text-2xl w-[90vw] mx-[5vw] mt-3 px-8 rounded-2xl outline-black bg- outline-[1px] outline backdrop-blur-sm">
-        <div className="flex justify-between max-w-5xl py-3 mx-auto">
-          <div>
-            <a href="/" className=" font-bold">ds.</a>
+      <header className="border-b border-tirtiaty w-full h-24 xl:h-36 flex items-center justify-between">
+        <a
+          href="/"
+          className="w-14 h-14 xl:w-20 xl:h-20 ml-8 xl:ml-14 block text-seconday-100"
+        >
+          <Logo />
+        </a>
+
+        <div className="flex justify-end h-full font-bold text-xl xl:text-2xl">
+          <div className="hidden h-full xl:flex items-center space-x-4 px-20 border-l border-tirtiaty">
+            <p>Amritsar, IN</p>
           </div>
-          <div className="r font-light text-[.75em]">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-          </div>
+
+          <button
+            className="h-full flex items-center space-x-4 px-8 xl:px-20 border-l border-tirtiaty"
+            onClick={() => {
+              // Open the menu
+            }}
+          >
+            {MenuIcon}
+            <p>Menu</p>
+          </button>
         </div>
       </header>
     </>
