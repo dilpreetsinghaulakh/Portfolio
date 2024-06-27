9bcd4d8e9c5bc41fc1a4a7aa29679143d07fa008
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-06-27 17:04:47 +0530

    feat: Add responsive menu to Header component
    - Implement a responsive menu in the Header component for improved navigation on smaller screens
    - Add menu icon and overlay functionality to display menu options
    - Update Header component to include the MenuContent and OverlayControl components

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,7 +1,95 @@
 "use client";
+import { use, useEffect, useRef, useState } from "react";
 import Logo from "../../../public/logo.svg";
+import Overlay from "./Overlay";
+import { X } from "react-feather";
+
+function MenuContent({
+  setIsMenuOpen,
+}: {
+  setIsMenuOpen: (value: boolean) => void;
+}) {
+  const content = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      content.current?.classList.remove("h-0");
+      content.current?.classList.add("h-full");
+    }, 1);
+  }, []);
+
+  function Link({
+    href,
+    delay,
+    children,
+  }: {
+    href: string;
+    delay: number;
+    children: string;
+  }) {
+    const linkRef = useRef<HTMLAnchorElement>(null);
+
+    setTimeout(() => {
+      linkRef.current?.classList.remove("opacity-0");
+    }, 100 + delay);
+    return (
+      <a
+        ref={linkRef}
+        href={href}
+        className="text-6xl opacity-0 transition-opacity duration-700"
+        onClick={() => setIsMenuOpen(false)}
+      >
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <div
+      ref={content}
+      className="flex flex-col items-center justify-center gap-8 bg-black w-full h-0 transition-all duration-700"
+    >
+      <button
+        className="absolute top-0 right-0 flex items-center justify-center gap-2 h-24 xl:h-36 px-8 xl:px-20 border-b border-l bg-black border-neutral-700 font-bold text-2xl"
+        onClick={() => {
+          content.current?.classList.remove("h-full");
+          content.current?.classList.add("opacity-0");
+          content.current?.classList.add("h-0");
+          content.current?.classList.add("overflow-hidden");
+          setTimeout(() => {
+            setIsMenuOpen(false);
+          }, 700);
+        }}
+      >
+        <X className="w-8 h-8 xl:w-14 xl:h-12 stroke-[1.5px]" />
+        Close
+      </button>
+      <Link href="/" delay={0}>
+        Home
+      </Link>
+      <Link href="/contact" delay={300}>
+        Contact
+      </Link>
+    </div>
+  );
+}
+
+function OverlayControl({
+  isMenuOpen,
+  setIsMenuOpen,
+}: {
+  isMenuOpen: boolean;
+  setIsMenuOpen: (value: boolean) => void;
+}) {
+  return (
+    <Overlay isOpen={isMenuOpen}>
+      <MenuContent setIsMenuOpen={setIsMenuOpen} />
+    </Overlay>
+  );
+}
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const MenuIcon = (
     <svg className="w-8 h-8 xl:w-12 xl:h-12" viewBox="0 0 50 50">
       <line
@@ -24,29 +112,31 @@ export default function Header() {
   );
   return (
     <>
-      <header className="border-b border-tirtiaty w-full h-24 xl:h-36 flex items-center justify-between">
+      <header className="border-b border-tertiary w-full h-24 xl:h-36 flex items-center justify-between">
         <a
           href="/"
+          aria-label="Home page link"
           className="w-14 h-14 xl:w-20 xl:h-20 ml-8 xl:ml-14 block text-secondary-100"
         >
           <Logo />
         </a>
 
         <div className="flex justify-end h-full font-bold text-xl xl:text-2xl">
-          <div className="hidden h-full xl:flex items-center space-x-4 px-20 border-l border-tirtiaty">
+          <div className="hidden h-full xl:flex items-center space-x-4 px-20 border-l border-tertiary">
             <p>Amritsar, IN</p>
           </div>
 
           <button
-            className="h-full flex items-center space-x-4 px-8 xl:px-20 border-l border-tirtiaty"
+            className="h-full flex items-center space-x-4 px-8 xl:px-20 border-l border-tertiary"
             onClick={() => {
-              // Open the menu
+              setIsMenuOpen(true);
             }}
           >
             {MenuIcon}
             <p>Menu</p>
           </button>
         </div>
+        <OverlayControl isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       </header>
     </>
   );
