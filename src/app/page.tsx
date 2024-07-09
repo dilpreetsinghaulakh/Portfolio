a7ec1a05bb48702277d5a1940e465598f834a3d6
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-07-09 12:43:26 +0530

    refactor: Remove lenis smooth scroll from home page
    Remove the lenis smooth scroll functionality from the home page as it is no longer being used due to performance issues. This improves the codebase by removing unnecessary dependencies and simplifying the code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,11 @@
-"use client";
 import CatchLine from "@/components/Home/CatchLine";
 import Contact from "@/components/Home/Contact";
 import HeroSection from "@/components/Home/HeroSection";
 import Projects from "@/components/Home/Projects";
 import TechStack from "@/components/Home/TechStack";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Lenis from "lenis";
-
-const lenis = new Lenis();
-
-gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function Page() {
-  useGSAP(() => {
-    lenis.on("scroll", ScrollTrigger.update);
 
-    gsap.ticker.add((time) => {
-      lenis.raf(time * 1000);
-      lenis.options.syncTouch = false;
-    });
-
-    gsap.ticker.lagSmoothing(0);
-  });
   return (
     <>
       <HeroSection />
