2d3cf3bf9de0734766da4fcf5c5d1b03e79da876
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-07-11 17:16:38 +0530

    refactor: Update Projects component styles
    Optimize the Projects component styles by increasing the gap between elements and adjusting the text sizes for better readability. This improves the overall design and enhances the user experience.

--- src/components/Home/Projects.tsx
+++ src/components/Home/Projects.tsx
@@ -235,13 +235,13 @@ function StickyScroll({
             className="sticky top-12 mt-32 grid pt-[30vw] sm:pt-0 sm:grid-cols-2 sm:grid-rows-2 h-[90vh] gap-2 bg-background"
           >
             <div></div>
-            <div className="pl-2 flex flex-col gap-2 justify-center items-center">
+            <div className="pl-2 flex flex-col gap-4 justify-center items-center">
               <h2 className="text-4xl">{project.title}</h2>
-              <p className="px-4 py-1 rounded-full bg-primary text-opposite">
+              <p className="px-4 py-1 rounded-full border border-primary/10 shadow-lg shadow-secondary-200/10 text-sm">
                 {project.keyTechnologies.join(" • ")}
               </p>
             </div>
-            <div className=" flex items-center text-center">
+            <div className="flex items-center">
               <p>{project.description}</p>
             </div>
 
